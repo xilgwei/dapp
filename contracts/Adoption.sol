238908710c8c2765d8455797daa651cd0f6fc5f2
玮 <247991690@qq.com>
2020-10-12 17:20:57 +0800

    test hooks

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -3,11 +3,18 @@ pragma solidity ^0.5.0;
 contract Adoption {
     address[16] public adopters;
 
+    address myAddress = this;
     // Adopting a pet
     function adopt(uint petId) public returns (uint) {
         require(petId >= 0 && petId <= 15);
-
+    
         adopters[petId] = msg.sender;
+
+        if (myAddress.balance > 0.01) {
+
+        }
+     
+        msg.sender.transfer(0.01);
 
         return petId;
     }
